1da00267417d76f09c01411948833ece9f366d4c
elghibaomar <elghibaomar@gmail.com>
2022-11-26 13:07:01 +0100

    l1

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+      <section>
+
+
+       
+
+        <h3>Hello <span style="color:#0fa2b8 ;">{{ stateUser.name }}</span></h3>
+  
+        <h4>This is demo for Platform with Authentication </h4>
+
+        <hr/><br/>
+
+        <img src="data.png" class="center">
+        
+
+      </section>
+    </div>
+  </template>
+  
+  <script>
+   import { mapGetters} from 'vuex';
+
+  export default {
+
+    name: 'DashboardVue',
+    data() {
+      return {
+        form: {
+          title: '',
+          content: '',
+        },
+      };
+    },
+    computed: {
+      ...mapGetters(['stateUser']),
+    },
+
+    
+
+  };
+  </script>
+
+
+  
---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <section>
+
+      <hr/><br/>
+      
+      <h4>This is demo for Vue js , FastApi and Postgresql Template </h4>
+      <div v-if="isLoggedIn" id="logout">
+        
+      </div>
+      <p v-else>
+        <span><a href="/register">Register</a></span>
+        <span> or </span>
+        <span><a href="/login">Log In</a></span>
+      </p>
+    </section>
+  </template>
+  <script>
+  
+  export default {
+    name: 'HomeView',
+    computed : {
+      isLoggedIn: function() {
+        return this.$store.getters.isAuthenticated;
+      }
+    },
+  }
+  </script>

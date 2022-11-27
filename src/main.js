2874d3024b6051504e0df202586c14e5b8052e3e
elghibaomar <elghibaomar@gmail.com>
2022-11-27 21:55:34 +0100

    l1

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import store from './store';
 
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = 'http://127.0.0.1:8000/';
+axios.defaults.baseURL = 'http://34.82.16.129/';
 
 Vue.config.productionTip = false;
 
